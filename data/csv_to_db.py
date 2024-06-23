1448c018328824a09badefa4d0f8193fd5233a13
shamshudeenj <j.shamshudeen@gmail.com>
2024-06-23 20:00:40 +0530

    adds csv to db conversaion file

--- data/csv_to_db.py
+++ data/csv_to_db.py
@@ -1,9 +1,16 @@
-import pandas as pd
 import sqlite3
+import pandas as pd
+
+with open('data/data.db','wb+') as file_obj:
+    file_obj.close()
 
 con = sqlite3.connect('data/data.db')
+
 df_invest = pd.read_csv('data/inve.csv')
 df_invest['date'] = pd.to_datetime(df_invest['date']).dt.date
+
 print(df_invest.info())
 print(df_invest)
-# df_invest.to_sql('investment',con, if_exists='append', index=False)
+
+df_invest.to_sql('investment',con, if_exists='append', index=False)
+print('Investment data successfully stored in DB!')
